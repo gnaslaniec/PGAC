a162a61a79c4765202a4754b45d78fa7145058f5
Gabriel Jose Naslaniec <gabrielnaslaniec@brq.com>
2020-09-26 10:45:58 -0300

    Alterado BD para PostgreSQL e refatoração do código

--- operacoes.py
+++ operacoes.py
@@ -1,4 +1,19 @@
+import psycopg2
+
 class Operacoes(object):
+
+    def conexao_bd():
+        con = psycopg2.connect(
+            database="pgac",
+            user="postgres",
+            password="root",
+            host="localhost",
+            port="5432"
+        )
+
+        cur = con.cursor()
+
+        return con, cur
 
     def retorna_saldo_usuario(cursor, id_usuario):
         cursor.execute("""select saldo from usuario where id = %s""", (id_usuario,))
@@ -6,6 +21,6 @@ class Operacoes(object):
         return saldo[0]
 
     def atualiza_saldo(db,cursor,saldo,id_usuario):
-        saldo_atualizado = saldo - 4.50
+        saldo_atualizado = saldo - 450
         cursor.execute("""update usuario set saldo = %s where id = %s""", (saldo_atualizado,id_usuario))
         db.commit()
--- pgac.py
+++ pgac.py
@@ -1,7 +1,6 @@
-from flask import Flask, flash, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_qrcode import QRcode
-from flask_mysqldb import MySQL
-import mysql.connector
+import psycopg2
 from serial import Serial
 from operacoes import Operacoes
 import time
@@ -9,25 +8,20 @@ import time
 # Comunicação serial com o Arduino/Catraca
 ser = Serial('COM3', 9600)
 time.sleep(2)
+
 # Configurações do Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Jp8fSDuJBD9dklluvxk2cQ'
 qrcode = QRcode(app)
-MySQL(app)
+
 #Conexão com o Banco de dados
-db = mysql.connector.connect(
-   host="localhost",
-   user="root",
-   passwd="root",
-   database='pgac',
-   auth_plugin='mysql_native_password'
-)
-cursor = db.cursor(buffered=True)
+connection, cursor = Operacoes.conexao_bd()
 
 # Define a página inicial da aplicação
 @app.route("/", methods = ['POST','GET'])
 def index():
     return render_template("index.html")
+
 # API para autenticação e comunicação com a catraca
 @app.route('/autenticacao', methods=['POST'] )
 def autenticacao():
@@ -35,16 +29,15 @@ def autenticacao():
     id_usuario = req_data['id_usuario']
     nome = req_data['nome']
     saldo = Operacoes.retorna_saldo_usuario(cursor,id_usuario)
-    if saldo >= 4.50:
+    if saldo >= 450:
         ser.write(b'H')
         print("Saldo suficiente!")
-        Operacoes.atualiza_saldo(db,cursor,saldo,id_usuario)
+        Operacoes.atualiza_saldo(connection,cursor,saldo,id_usuario)
         return '1'
     else:
         ser.write(b'L')
         print("Saldo insuficiente!")
         return '0'
     
-
 if __name__ == "__main__":
     app.run(debug=True, port=3000, use_reloader=False)
