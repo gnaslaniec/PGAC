94a2c92f6b468660a520648b44778e78622dbf0a
Gabriel Jose Naslaniec <gabrielnaslaniec@brq.com>
2020-09-30 20:42:00 -0300

    Alteração de servidor Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python pgac.py runserver 0.0.0.0:5000
+web: gunicorn pgac:app
--- pgac.py
+++ pgac.py
@@ -55,4 +55,4 @@ def autenticacao():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 33507))
-    app.run(host='0.0.0.0', debug=False, port=port, use_reloader=False)
+    app.run(host='0.0.0.0', debug=True, port=port, use_reloader=False)
