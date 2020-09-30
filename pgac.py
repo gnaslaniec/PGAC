c147d33ec813f60ac2117149c985c713cd729c1e
Gabriel Jose Naslaniec <gabrielnaslaniec@brq.com>
2020-09-30 20:34:34 -0300

    Heroku zoado

--- pgac.py
+++ pgac.py
@@ -54,5 +54,5 @@ def autenticacao():
         return '0'
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', debug=False, port=port, use_reloader=False)
